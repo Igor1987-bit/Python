85839b831f600086fb2d2faf946e57ce9bcc10e3
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2024-12-26 20:14:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#print('Hi, PyCharm')
-#x = 43
-#y = 32
-#print(x * y)
-#print("End line")
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
 
-from time import sleep
-a = 5
-print(a)
-print('Я тута')
-sleep(15)
-print("Фух, 15 секунд прошло")
+# from time import sleep
+# a = 5
+# print(a)
+# print('Я тута')
+# sleep(15)
+# print("Фух, 15 секунд прошло")
