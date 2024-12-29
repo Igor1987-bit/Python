da0382dc68a64b2928a3c31d8d85fc6302faa235
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2024-12-29 18:33:27 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,1 +1,15 @@
-
+# import this
+print('''
+    Красота лучше, чем уродство.
+    Явное лучше, чем неявное.
+    Простота лучше, чем сложность.
+    Читаемость имеет значение.''')
+namder = int(input('Введите число: '))
+# print(name)
+if namder > 1:
+    print("OK")
+elif namder < -5:
+    print("ОГО! ", namder)
+x, y = 5, 2
+a = 2
+print(x, y, a)
