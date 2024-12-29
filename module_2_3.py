ae9d30e3f577dd0ad24b5c7df007e9d6bd888eec
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2024-12-29 22:05:37 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,18 +1,16 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while i < len(my_list):
-    if my_list[i] > 0:
-        print(my_list[i])
+    if my_list[i] == 0:
+        i = i + 1           # т.к. возврат к началу, i + 1
+        continue            # возврат к началу
+    elif my_list[i] < 0:    # завершение цикла
+        break
+    print(my_list[i])
     i = i + 1
-print("Задание выполнено, 'continue' и 'break' не использовано")
 # consol
 # 42
 # 69
 # 322
 # 13
 # 99
-# 9
-# 8
-# 7
-# 5
-# Задание выполнено, 'continue' и 'break' не использовано
