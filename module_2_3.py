e0728f2982df14ae435fecd442cd37a9a1eb00e5
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2024-12-30 20:05:00 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,13 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-i = 0
+i = -1
 while i < len(my_list):
+    i = i + 1
     if my_list[i] == 0:
-        i = i + 1           # т.к. возврат к началу, i + 1
         continue            # возврат к началу
     elif my_list[i] < 0:    # завершение цикла
         break
     print(my_list[i])
-    i = i + 1
 # consol
 # 42
 # 69
