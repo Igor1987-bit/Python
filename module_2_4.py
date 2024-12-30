c1393492e0afe1e16703e57d5c07ab2d667374ac
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2024-12-31 00:48:40 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,1 +1,20 @@
-
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+print("It's not that simple")
+for x1 in numbers:
+    if x1 == 1:
+        continue
+    number_prime = True
+    for x2 in range(2, x1):
+        if x1 % x2 == 0:
+            number_prime = False
+            break
+    if number_prime:
+        primes.append(x1)
+    else:
+        not_primes.append(x1)
+print("Prime numbers:")
+print(primes)
+print("Not prime numbers:")
+print(not_primes)
