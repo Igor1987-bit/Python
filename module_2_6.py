06358f191244e2e89239fe7f2cce96ebd73ad5d4
Igor1987-bit <40392992+Igor1987-bit@users.noreply.github.com>
2025-01-01 21:04:01 +0300

    Update module_2_6.py

--- module_2_6.py
+++ module_2_6.py
@@ -1,1 +1,22 @@
+print('Very old cipher')
+def password(number):
+    passcode = ''
+    for i in range(1, number):
+        j = 0
+        while j <= number:
+            j += 1
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                passcode += str(i) + str(j)
+    return passcode
 
+number = int(input('Enter a number from 3 to 20: '))
+if number >= 3 and number <= 20:
+    print(password(number))
+else:
+    print('the number is not in range [3:20]')
+console
+# Very old cipher
+# Enter a number from 3 to 20: 7
+# 162534
